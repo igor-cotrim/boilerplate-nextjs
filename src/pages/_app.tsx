ee9094fcf40d3cd28ec15f8dbbf5c319dbd6bb93
Igor Cotrim <igorcotrim.dev@gmail.com>
2022-03-08 22:00:33 -0300

    config pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ function App({ Component, pageProps }: AppProps) {
         <title>Boilerplate NextJS</title>
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/vercel.svg" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
