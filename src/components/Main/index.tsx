814ba5f1cf306792d4aaee499097c15e85e19772
Igor Cotrim <igorcotrim.dev@gmail.com>
2022-03-08 21:46:26 -0300

    install and config storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,8 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({ title = 'Boilerplate NextJS' }) => (
   <S.Wrapper>
-    <S.Title>Boilerplate NextJS</S.Title>
+    <S.Title>{title}</S.Title>
   </S.Wrapper>
 )
 
